a277f02ac646aa5beec13f0b1cd91311f0f6cc23
Tilman Adler <tilman-git@t-animal.de>
2020-08-26 23:56:31 +0200

    Move modules to modules folder

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
-import {loadAndDisplayMap} from './map.js';
-import { loadAndDisplayCountyList } from './county-list';
-import {loadAndDisplayDataAge} from './data-age';
-import { loadAndDisplaySums } from './summed-data.js';
+import {loadAndDisplayMap} from './modules/map.js';
+import { loadAndDisplayCountyList } from './modules/county-list';
+import {loadAndDisplayDataAge} from './modules/data-age';
+import { loadAndDisplaySums } from './modules/summed-data.js';
 
 loadAndDisplayMap();
 loadAndDisplayCountyList();
