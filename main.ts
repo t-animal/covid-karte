fd605bb5f11ce701aa3afc48b273bd5759e66cd0
Tilman Adler <tilman-git@t-animal.de>
2020-08-30 22:20:25 +0200

    Rename charts-by-day folder to simply charts

--- main.ts
+++ main.ts
@@ -2,8 +2,8 @@ import { loadAndDisplayMap } from './modules/map/map.js';
 import { loadAndDisplayCountyList } from './modules/county-list';
 import { loadAndDisplayDataAge } from './modules/data-age';
 import { loadAndDisplaySums } from './modules/summed-data.js';
-import { loadAndDisplayDailyInfections } from './modules/by-day-charts/daily-infections';
-import { initNavigation } from './modules/by-day-charts/section-nav';
+import { loadAndDisplayDailyInfections } from './modules/charts/daily-infections';
+import { initNavigation } from './modules/charts/section-nav';
 
 import * as countyInfo from './modules/county-information';
 
