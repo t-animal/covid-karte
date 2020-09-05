68b29e5c90e1e32fbbabeda40c4f62a4eebebcec
Tilman Adler <tilman-git@t-animal.de>
2020-09-05 18:26:16 +0200

    Cache formatters to improve performance

--- modules/helpers.ts
+++ modules/helpers.ts
@@ -19,6 +19,16 @@ export async function countyNameById(countyId: number): Promise<string | null> {
     ?.attributes.county ?? null;
 }
 
+
+const formatters: {[decimalDigits: number]: Intl.NumberFormat} = {};
 export function format(num: number, decimalDigits = 0): string {
-  return new Intl.NumberFormat('de-DE', {  maximumFractionDigits: decimalDigits, }).format(num);
+  return (formatters[decimalDigits] ?? makeFormatter(decimalDigits)).format(num);
+}
+
+function makeFormatter(decimalDigits: number) {
+  formatters[decimalDigits] = new Intl.NumberFormat('de-DE', {
+    maximumFractionDigits: decimalDigits
+  });
+
+  return formatters[decimalDigits];
 }
