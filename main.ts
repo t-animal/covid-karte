7b3a34a57b7084df688e2f0cc8f118a41e664f7a
Tilman Adler <tilman-git@t-animal.de>
2020-09-06 11:47:41 +0200

    Store selected county in url

--- main.ts
+++ main.ts
@@ -3,6 +3,7 @@ import * as cumulativeCasesPerDay from './modules/charts/cumulative-cases-per-da
 import * as dailyCasesByReportday from './modules/charts/daily-cases-by-reportday';
 import * as dailyCasesBySickday from './modules/charts/daily-cases-by-sickday';
 import * as summedData from './modules/summed-data.js';
+import * as countySelectionPersistence from './modules/county-selection-persistence';
 
 import { initNavigation } from './modules/charts/section-nav';
 import { loadAndDisplayCountyList } from './modules/county-list';
@@ -10,6 +11,9 @@ import { loadAndDisplayDataAge } from './modules/data-age';
 import { loadAndDisplayMap } from './modules/map/map.js';
 import { loadAndDisplaySums } from './modules/summed-data.js';
 import { loadAndRenderDailyCasesBySickday } from './modules/charts/daily-cases-by-sickday';
+import { restoreCountySelectionFromUrl } from './modules/county-selection-persistence';
+
+restoreCountySelectionFromUrl();
 
 loadAndDisplayMap();
 loadAndDisplayCountyList();
@@ -23,3 +27,4 @@ summedData.reactToCountySelection();
 dailyCasesBySickday.reactToCountySelection();
 dailyCasesByReportday.reactToCountySelection();
 cumulativeCasesPerDay.reactToCountySelection();
+countySelectionPersistence.reactToCountySelection();
---
+++ modules/county-selection-persistence.ts
@@ -0,0 +1,17 @@
+import { observeCountyChanges, selectCounty, selectedCountyRkiId } from './county-selection';
+
+export function reactToCountySelection(): void {
+  observeCountyChanges(() => {
+    const countyId = selectedCountyRkiId();
+    location.hash = countyId?.toString() ?? '';
+  });
+}
+
+export function restoreCountySelectionFromUrl(): void {
+  if(location.hash === '' || location.hash.match(/^#[0-9]+/) === null) {
+    return;
+  }
+
+  const countyId = parseInt(location.hash.substr(1));
+  selectCounty(countyId);
+}
