f3d8e6f3ecb71af1b7af7dce5db71e5b981af125
Tilman Adler <tilman-git@t-animal.de>
2020-10-16 23:01:21 +0200

    Adapt color scheme to changed rki color scheme

--- src/modules/map/map-helpers.ts
+++ src/modules/map/map-helpers.ts
@@ -129,7 +129,7 @@ export function color(sevenDaysInfectionsPer100k: number | undefined): string {
   if (sevenDaysInfectionsPer100k <= 5) return '#d7d3af';
   if (sevenDaysInfectionsPer100k <= 25) return '#d7d288';
   if (sevenDaysInfectionsPer100k <= 50) return '#cd9406';
-  if (sevenDaysInfectionsPer100k <= 250) return '#af2632';
+  if (sevenDaysInfectionsPer100k <= 100) return '#af2632';
   if (sevenDaysInfectionsPer100k <= 500) return '#8c0619';
   return '#fff';
 }
