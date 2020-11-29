651104636b8ee9c75457153c6f9c4c9d229e6eb5
Tilman Adler <tilman-git@t-animal.de>
2020-11-29 11:46:35 +0100

    Update color schemes

--- src/modules/map/label-scheme.ts
+++ src/modules/map/label-scheme.ts
@@ -4,31 +4,34 @@ import { LabelScheme, loadSettings } from '../settings';
 type ColorSchemeData = {max: number, color: string}[]
 
 const rkiScheme: ColorSchemeData = [
-  {max: 0, color: '#d2d2d2' },
-  {max: 5, color: '#d7d3af' },
-  {max: 25, color: '#d7d288' },
-  {max: 50, color: '#cd9406' },
-  {max: 100, color: '#af2632' },
-  {max: 500, color: '#8c0619' },
+  { max: 0,    color: '#ccf5c4' },
+  { max: 5,    color: '#faf7c9' },
+  { max: 25,   color: '#faee7d' },
+  { max: 50,   color: '#fab133' },
+  { max: 100,  color: '#d03523' },
+  { max: 250,  color: '#921214' },
+  { max: 500,  color: '#651212' },
+  { max: 1000, color: '#d80182' }
 ];
 
 const riskLayerScheme: ColorSchemeData = [
-  {max: 0.01, color: '#2c83b9'},
-  {max: 15, color: '#80d38d'},
-  {max: 25, color: '#feffb1'},
-  {max: 35, color: '#feca81'},
-  {max: 50, color: '#f1894a'},
-  {max: 100, color: '#eb1a1f'},
-  {max: 200, color: '#ac1316'},
-  {max: 350, color: '#b275dd'},
-  {max: 500, color: '#5b189b'},
+  { max: 0.01, color: '#2c83b9' },
+  { max: 15,   color: '#80d38d' },
+  { max: 25,   color: '#feffb1' },
+  { max: 35,   color: '#feca81' },
+  { max: 50,   color: '#f1894a' },
+  { max: 100,  color: '#eb1a1f' },
+  { max: 200,  color: '#ac1316' },
+  { max: 350,  color: '#b275dd' },
+  { max: 500,  color: '#5b189b' },
+  { max: 1000, color: '#222222' }
 ];
 
 export function colorForIncidence(sevenDaysInfectionsPer100k: number | undefined): string {
   return getColor(getSelectedColorScheme(), sevenDaysInfectionsPer100k);
 }
 
-export function renderColorScheme() {
+export function renderColorScheme(): void {
   const renderBox = (color: string) =>  {
     const div = document.createElement('div');
     div.style.backgroundColor = color;
