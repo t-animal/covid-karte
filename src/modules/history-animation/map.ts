a443d7954beee3c748779c252d06a4b6b5037a47
Tilman Adler <Tilman.Adler@methodpark.de>
2021-02-02 21:47:37 +0100

    Make callbacks available to dev namespace

--- src/modules/history-animation/map.ts
+++ src/modules/history-animation/map.ts
@@ -1,23 +1,10 @@
-import {
-  loadHistoricCountyData
-} from '../data-loading';
-import { RkiCountyFeatureAttributes, RkiFeatureData } from '../data-loading/types';
-import { daysSince } from '../helpers';
-import { addCountiesToMap, highlightSelectedCounty } from '../map/map';
-import { loadCountyMap } from '../map/map-helpers';
+import * as animation from './animation';
+import { selectDate, selectToday } from './date-selection';
 
 export function addDev() {
-  (window as any).loadAndDisplayHistoric = async (): Promise<void> => {
-    for (const [year, month, day] of daysSince(2020, 9, 1)) {
-      const rkiData: RkiFeatureData<RkiCountyFeatureAttributes> =
-        await loadHistoricCountyData(year, month, day);
-      const countiesGeoJson = await loadCountyMap();
-
-      addCountiesToMap(rkiData, countiesGeoJson);
-
-      highlightSelectedCounty(rkiData, countiesGeoJson);
-
-      await new Promise(resolve => setTimeout(resolve, 0));
-    }
+  (window as any).dev = {
+    animation,
+    selectDate,
+    selectToday
   };
 }
