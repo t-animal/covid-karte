96f56a93e0a06dd82946db97140ed6dc3de0ac72
Tilman Adler <Tilman.Adler@methodpark.de>
2021-02-21 10:58:22 +0100

    Add precaching hint to map

--- src/modules/history-animation/animation.ts
+++ src/modules/history-animation/animation.ts
@@ -1,10 +1,13 @@
-import { daysSince } from '../helpers';
+import { loadHistoricCountyData } from '../data-loading';
+import { daysSince, getElementOrThrow } from '../helpers';
 import { getDate, selectDate, selectToday } from './date-selection';
 
 let cancel = false;
 
 export async function runAnimation(): Promise<void> {
   cancel = false;
+
+  await cacheForAnimation();
 
   document.querySelector('body').dataset.runningAnimation = '';
 
@@ -24,6 +27,30 @@ export async function runAnimation(): Promise<void> {
   }
 
   delete document.querySelector('body').dataset.runningAnimation;
+}
+
+async function cacheForAnimation() {
+  const loadingPromises = [...daysSince(2020, 9, 1)].map((date) => loadHistoricCountyData(...date));
+
+  await showCachingHint(loadingPromises);
+}
+
+async function showCachingHint(promises: Promise<any>[]) {
+  const elem = getElementOrThrow('.animation-caching-hint');
+
+  elem.parentElement.classList.add('precaching');
+
+  let promisesDone = 0;
+  for (const promise of promises) {
+
+    elem.textContent = `Bitte warten. Vorausladen von ${promisesDone} von
+      ${promises.length} Tagesdatensätzen fertig.`;
+
+    await promise;
+    promisesDone++;
+  }
+
+  elem.parentElement.classList.remove('precaching');
 }
 
 export function cancelAnimation(): void {
