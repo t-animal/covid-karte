f046fc4cd12f0a8e0e872326152c8788b0a8b702
Tilman Adler <tilman-git@t-animal.de>
2021-04-18 14:27:33 +0200

    Add seven day incidence display

--- src/modules/summed-data/summed-data.ts
+++ src/modules/summed-data/summed-data.ts
@@ -1,8 +1,11 @@
 
-import { observeCountyChanges, selectedCountyRkiId } from '../county-selection';
+import {
+  getDataOfSelectedCounty, observeCountyChanges, selectedCountyRkiId
+} from '../county-selection';
 import {
   loadCountyData, loadTodaysCasesDiff, loadTodaysDeathsDiff, loadTodaysSummedData
 } from '../data-loading';
+import { loadTodaysAggregateData } from '../data-loading/index';
 import { format, getElementOrThrow } from '../helpers';
 import { renderDiffData } from './generic-rendering';
 
@@ -12,6 +15,7 @@ export function loadAndDisplaySums(): void {
   loadAndDisplayCountySpecificCasesAndDeathSums(countyId);
   loadAndDisplayDeathDiff(countyId);
   loadAndDisplayCasesDiff(countyId);
+  loadAndDisplay7DayIncidence(countyId);
 }
 
 export async function reactToCountySelection(): Promise<void> {
@@ -67,3 +71,23 @@ async function loadAndDisplayCasesDiff(countyId: number | null) {
   renderDiffData(section, data, countyId);
 }
 
+async function loadAndDisplay7DayIncidence(countyId: number | null) {
+  const data = await loadTodaysAggregateData();
+
+  const section = getElementOrThrow('.seven-day-incidence-section');
+  const primary = getElementOrThrow('.seven-day-incidence-section .seven-day-incidence', section);
+  const secondary = getElementOrThrow('.secondary-info', section);
+
+  if(countyId == null) {
+    primary.textContent = format(data.features[0].attributes.Inz7T, 1);
+    secondary.classList.add('no-data');
+  } else {
+    const countyData = await getDataOfSelectedCounty();
+    
+    primary.textContent = format(countyData?.attributes.cases7_per_100k ?? -1, 1);
+    getElementOrThrow('span', secondary).textContent = format(data.features[0].attributes.Inz7T, 1);
+    secondary.classList.remove('no-data');
+  }
+
+  
+}
